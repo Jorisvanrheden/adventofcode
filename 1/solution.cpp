6aca7a11df631e2a0c5c30138441158c9fda11d9
Joris van Rheden <jorisvanrheden@hotmail.com>
2021-12-13 17:53:07 +0100

    solution of day 1 - part 2

--- 1/solution.cpp
+++ 1/solution.cpp
@@ -29,15 +29,30 @@ std::vector<int> parseInput(std::string filepath)
     return input;
 }
 
+int findIncreasingMeasurements(const std::vector<int>& input)
+{
+    int counter = 0;
+
+    //start from index 1, as index 0 has no value to compare with
+    for(int i=1;i<input.size();i++)
+    {
+        int current = input[i];
+        int previous = input[i-1];
+
+        if(current > previous) counter++;
+    }
+
+    return counter;
+}
+
 int main()
 {
-    std::cout<<"Hello world"<<std::endl;
-
     std::vector<int> input = parseInput("./input");
 
-    std::cout<<input.size()<<std::endl;
+    int result = findIncreasingMeasurements(input);
+
+    std::cout<<"--- ANSWER IS ---"<<std::endl;
+    std::cout<<result<<std::endl;
 
     return 0;
 }
-
-//Parse input
