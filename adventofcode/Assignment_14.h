ea10b55497e8d92922d8ff323ada078d8d1d46c8
Joris van Rheden <jorisvanrheden@hotmail.com>
2021-12-24 11:38:01 +0100

    day 14 part 2

--- adventofcode/Assignment_14.h
+++ adventofcode/Assignment_14.h
@@ -58,39 +58,60 @@ private:
         return Input(entries, sequence);
     }
 
-    std::string process(const std::string& input, const std::vector<Entry>& entries) 
+    void process_2(std::map<std::string, unsigned long long>& map, std::map<char, unsigned long long>& indexedMap, const std::vector<Entry>& entries)
     {
-        std::string modifiedInput = input;
+        //keep track of symbols and their inject-value
+        std::map<std::string, std::string> symbolMap;
+        std::map<std::string, unsigned long long> additions;
 
-        std::map<std::string, std::string> entryMap;
-        for (const auto& entry : entries) 
+        for (const auto& entry : entries)
         {
-            entryMap[entry.before] = entry.after;
+            //initialize symbol mapping
+            symbolMap[entry.before] = entry.after;
+            additions[entry.before] = 0;
         }
 
-        //keep track of the indices and what their inject value should be
-        std::map<unsigned long long, std::string> map;
-
-        for (int i = 0; i < input.length() - 1; i++) 
+        //keep track of how many times each combination occurs
+        for (const auto& pair : map)
         {
-            std::string pair;          
-            pair += input[i];
-            pair += input[i + 1];
+            if (pair.second == 0) continue;
+
+            std::string symbol = pair.first;
 
             //check if the pair is in the sequence
-            if (entryMap.find(pair) != entryMap.end()) 
+            if (symbolMap.find(symbol) != symbolMap.end())
             {
-                int index = i + map.size() + 1;
-                map[index] = entryMap.at(pair);
-            }
+                std::string symbolToInject = symbolMap.at(symbol);
+
+                //also increment the two created new values
+                std::string valueA;
+                valueA += symbol[0];
+                valueA += symbolToInject;
+
+                std::string valueB;
+                valueB += symbolToInject;
+                valueB += symbol[1];
+
+                additions[valueA] += pair.second;   
+                additions[valueB] += pair.second;
+
+                //process the indexed map additions
+                if (indexedMap.find(symbolToInject[0]) == indexedMap.end())
+                {
+                    indexedMap[symbolToInject[0]] = 0;
+                }
+                indexedMap[symbolToInject[0]] += pair.second;
+
+                //since the original combination doesn't exist anymore, remove it
+                map[symbol] -= pair.second;
+            }                       
         }
 
-        for (const auto& pair : map) 
+        //add combination count in the map
+        for (const auto& pair : additions) 
         {
-            modifiedInput.insert(pair.first, pair.second);
+            map[pair.first] += pair.second;
         }
-
-        return modifiedInput;
     }
 
     std::map<unsigned long long, char> flipMap(const std::map<char, unsigned long long>& map)
@@ -105,39 +126,49 @@ private:
         return flippedMap;
     }
 
-    std::map<char, unsigned long long> indexSequence(const std::string& sequence)
-    {
-        std::map<char, unsigned long long> map;
-
-        for (int i = 0; i < sequence.length(); i++) 
-        {
-            if (map.find(sequence[i]) == map.end()) 
-            {
-                map[sequence[i]] = 0;
-            }
-
-            map[sequence[i]]++;
-        }
-        
-        return map;
-    }
-
     unsigned long long getResultAfterSteps(Input input, int steps) 
     {
-        std::string sequence = input.sequence;
+        //initialize map and indexed map
+        std::map<std::string, unsigned long long> map;
+        std::map<char, unsigned long long> indexedMap;
+
+        for (const auto& entry : input.entries)
+        {
+            //initialize the map counter 
+            map[entry.before] = 0;
+        }
+        for (const auto& c : input.sequence)
+        {
+            //initialize the indexed map counter 
+            if (indexedMap.find(c) == indexedMap.end()) 
+            {
+                indexedMap[c] = 0;
+            }
+
+            indexedMap[c]++;
+        }
+
+        for (int i=0;i<input.sequence.size()-1;i++)
+        {
+            std::string symbol;
+            symbol += input.sequence[i];
+            symbol += input.sequence[i + 1];
+
+            map[symbol]++;
+        }
+
         for (int i = 0; i < steps; i++)
         {
             std::cout << "Processing day " << i << std::endl;
-            sequence = process(sequence, input.entries);
+            process_2(map, indexedMap, input.entries);
         }
 
-        std::map<char, unsigned long long> map = indexSequence(sequence);
-        std::map<unsigned long long, char> sortedMap = flipMap(map);
+        std::map<unsigned long long, char> flippedMap = flipMap(indexedMap);
 
-        char leastCommonChar = sortedMap.begin()->second;
-        char mostCommonChar = std::prev(sortedMap.end())->second;
+        char leastCommonChar = flippedMap.begin()->second;
+        char mostCommonChar = std::prev(flippedMap.end())->second;
 
-        return map.at(mostCommonChar) - map.at(leastCommonChar);
+        return indexedMap.at(mostCommonChar) - indexedMap.at(leastCommonChar);
     }
 
     std::string getSolutionPart1()
