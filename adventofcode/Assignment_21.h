26f2e8f3730f8d30cb880081980d47ccc4116c35
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-01-06 00:25:17 +0100

    day 21 part 2 first attempt setup

--- adventofcode/Assignment_21.h
+++ adventofcode/Assignment_21.h
@@ -13,6 +13,8 @@ private:
         int position;
         int score;
 
+        bool init = false;
+
         Player(int position, int score)
             :position(position), score(score)
         {
@@ -55,15 +57,7 @@ private:
             die.value = 1;
         }
 
-        
-
         die.rollCount++;
-
-        if (die.rollCount == 993)
-        {
-            std::cout << "here" << std::endl;
-        }
-
 
         return die.value;
     }
@@ -83,18 +77,74 @@ private:
         player.score += player.position;
     }
 
-    void playGame(std::vector<Player>& players, Die& die)
+    std::vector<Player> playGame(std::vector<Player> players, Die& die, int maxScore)
     {
         while (true)
         {
-            for (auto& player : data)
+            for (auto& player : players)
             {
                 processRound(player, die);
 
-                if (player.score >= 1000)
+                if (player.score >= maxScore)
                 {
+                    return players;
+                }
+            }
+        }
+
+        return players;
+    }
+
+    void processQuantumRound(std::vector<Player>& players, int playerIndex, std::vector<unsigned long long>& winnerCounts, int maxScore)
+    {
+        //first process the score?
+        if (players[playerIndex].position > 10)
+        {
+            while (players[playerIndex].position > 10) players[playerIndex].position -= 10;
+        }
+
+        if (players[playerIndex].init)
+        {
+            players[playerIndex].score += players[playerIndex].position;
+        }
+        else players[playerIndex].init = true;
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            //add 1, 2 or 3
+            //but for each of the added options, create a new quantum game 
+            //maybe initialize the player with the new value?
+            for (int dimension = 1; dimension <= 3; dimension++) 
+            {
+                std::vector<Player> copy = players;
+                copy[playerIndex].position += dimension;
+
+                playQuantumGame(copy, winnerCounts, maxScore);
+            }
+        }
+    }
+
+    //make a copy to have initialized values, but that they don't overwrite other 'universes'
+    void playQuantumGame(std::vector<Player> players, std::vector<unsigned long long>& winnerCounts, int maxScore)
+    {
+        //before playing, actually make sure there is no winner already
+        for (auto& player : players) 
+        {
+            if (player.score >= maxScore) return;
+        }
+
+        while (true)
+        {
+            for (int i = 0; i < players.size(); i++) 
+            {
+                if (players[i].score >= maxScore)
+                {
+                    winnerCounts[i]++;
                     return;
                 }
+
+                processQuantumRound(players, i, winnerCounts, maxScore);
             }
         }
     }
@@ -114,9 +164,9 @@ private:
         int result = 0;
 
         Die die(0);
-        playGame(data, die);
+        std::vector<Player> players = playGame(data, die, 1000);
 
-        result = getLosingScore(data, die);
+        result = getLosingScore(players, die);
 
         return std::to_string(result);
     }
@@ -125,6 +175,10 @@ private:
     {
         int result = 0;
 
+        std::vector<unsigned long long> winnerCounts(data.size(), 0);
+        playQuantumGame(data, winnerCounts, 21);
+
+        std::cout << std::to_string(winnerCounts[0]) << ", " << std::to_string(winnerCounts[1]) << std::endl;
         return std::to_string(result);
     }
 
@@ -133,7 +187,7 @@ private:
 public:
     void initialize()
     {
-        std::vector<std::string> input = Utilities::readFile("../21/input");
+        std::vector<std::string> input = Utilities::readFile("../21/input_example");
         data = parseInput(input);
     }
 };
