a5588cd13d65510bb68fcab5b490390592fc23e6
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-01-10 16:23:38 +0100

    day 21 part 2

--- adventofcode/Assignment_21.h
+++ adventofcode/Assignment_21.h
@@ -13,12 +13,21 @@ private:
         int position;
         int score;
 
-        bool init = false;
-
+        Player(){}
         Player(int position, int score)
             :position(position), score(score)
         {
 
+        }
+
+        void updateScore() 
+        {
+            if (position > 10)
+            {
+                while (position > 10) position -= 10;
+            }
+
+            score += position;
         }
     };
 
@@ -30,6 +39,24 @@ private:
         Die(int value)
             : value(value)
         {
+        }
+    };
+
+    struct Configuration
+    {
+        bool hasBeenPlayed = false;
+        std::string key;
+        std::vector<unsigned long long> results;
+
+        Configuration(const std::string& key, const std::vector<unsigned long long>& results)
+            : key(key), results(results), hasBeenPlayed(true)
+        {
+
+        }
+        Configuration(bool hasBeenPlayed)
+            : hasBeenPlayed(hasBeenPlayed)
+        {
+
         }
     };
 
@@ -67,14 +94,9 @@ private:
         for (int i = 0; i < 3; i++)
         {
             player.position += rollDie(die);
-
-            if (player.position > 10) 
-            {
-                while (player.position > 10) player.position -= 10;
-            }
+            
         }
-
-        player.score += player.position;
+        player.updateScore();
     }
 
     std::vector<Player> playGame(std::vector<Player> players, Die& die, int maxScore)
@@ -94,59 +116,35 @@ private:
 
         return players;
     }
-
-    void processQuantumRound(std::vector<Player>& players, int playerIndex, std::vector<unsigned long long>& winnerCounts, int maxScore)
+    
+    std::vector<Player> retrieveNextRoundPlayers(const Player& activePlayer) 
     {
-        //first process the score?
-        if (players[playerIndex].position > 10)
-        {
-            while (players[playerIndex].position > 10) players[playerIndex].position -= 10;
-        }
+        std::vector<Player> players(27);
 
-        if (players[playerIndex].init)
+        int count = 0;
+        for (int i = 1; i <= 3; i++)
         {
-            players[playerIndex].score += players[playerIndex].position;
-        }
-        else players[playerIndex].init = true;
-
-
-        for (int i = 0; i < 3; i++)
-        {
-            //add 1, 2 or 3
-            //but for each of the added options, create a new quantum game 
-            //maybe initialize the player with the new value?
-            for (int dimension = 1; dimension <= 3; dimension++) 
+            for (int j = 1; j <= 3; j++)
             {
-                std::vector<Player> copy = players;
-                copy[playerIndex].position += dimension;
-
-                playQuantumGame(copy, winnerCounts, maxScore);
-            }
-        }
-    }
-
-    //make a copy to have initialized values, but that they don't overwrite other 'universes'
-    void playQuantumGame(std::vector<Player> players, std::vector<unsigned long long>& winnerCounts, int maxScore)
-    {
-        //before playing, actually make sure there is no winner already
-        for (auto& player : players) 
-        {
-            if (player.score >= maxScore) return;
-        }
-
-        while (true)
-        {
-            for (int i = 0; i < players.size(); i++) 
-            {
-                if (players[i].score >= maxScore)
+                for (int k = 1; k <= 3; k++)
                 {
-                    winnerCounts[i]++;
-                    return;
-                }
+                    //create a copy of the active player
+                    Player player = activePlayer;
 
-                processQuantumRound(players, i, winnerCounts, maxScore);
+                    //override the position
+                    player.position += i + j + k;
+
+                    //process the score given the position of the player
+                    player.updateScore();
+
+                    //add the player to the list
+                    players[count] = player;
+
+                    count++;
+                }
             }
         }
+        return players;
     }
 
     int getLosingScore(const std::vector<Player>& players, const Die& die) 
@@ -157,6 +155,149 @@ private:
         }
 
         return 0;
+    }
+
+    unsigned long long getHighestWinningCount(const std::vector<unsigned long long>& results) 
+    {
+        unsigned long long highestResult = 0;
+        for (int i = 0; i < results.size(); i++) 
+        {
+            if (results[i] > highestResult) highestResult = results[i];
+        }
+        return highestResult;
+    }
+
+    std::string createKeyFromPlayer(const Player& player)
+    {
+        std::string key;
+
+        key += "[" + std::to_string(player.position) + "," + std::to_string(player.score) + "]";
+
+        return key;
+    }
+
+    std::string createKey(const std::vector<Player>& players, int playerIndex) 
+    {
+        std::string key;
+
+        for (const auto& player : players) 
+        {
+            key += createKeyFromPlayer(player);
+        }
+
+        key += std::to_string(playerIndex);
+
+        return key;
+    }
+
+    std::string createValue(std::vector<unsigned long long>& winnerCounts)
+    {
+        std::string value;
+
+        for (int i = 0; i < winnerCounts.size(); i++)
+        {
+            value += std::to_string(winnerCounts[i]);
+            if (i < winnerCounts.size() - 1) 
+            {
+                value += ",";
+            }
+        }
+
+        return value;
+    }
+
+    std::vector<unsigned long long> parseValue(const std::string& value) 
+    {
+        std::vector<unsigned long long> values;
+
+        std::vector<std::string> parts = Utilities::splitString(value, ",");
+        for (int i = 0; i < parts.size(); i++) 
+        {
+            values.push_back(std::stoll(parts[i]));
+        }
+
+        return values;
+    }    
+
+    //we have to find out how many games are played and the result given a certain start configuration
+    //with start configuration, we mean:
+    //- player 1 position
+    //- player 1 score
+    //- player 2 position
+    //- player 2 score
+
+    //then the result should be in the shape of:
+    //- amount games won by player 1
+    //- amount games won by player 2
+    Configuration getConfiguration(std::vector<Player>& players, int playerIndex, std::map<std::string, std::string>& map)
+    {
+        /* check if this 'game' configuration has been player before
+        if it has, simply add the results to the winnerCounts instead of playing it out again*/
+        std::string key = createKey(players, playerIndex);
+        if (map.find(key) != map.end())
+        {
+            std::string value = map.at(key);
+            std::vector<unsigned long long> values = parseValue(value);
+
+            return Configuration(value, values);
+        }
+        
+        return Configuration(false);
+    }
+
+    //make a copy to have initialized values, but that they don't overwrite other 'universes'
+    std::vector<unsigned long long> playQuantumGame(std::vector<Player>& players, int playerIndex, std::map<std::string, std::string>& map, int maxScore)
+    {
+        //first check if this specific configuration has already been executed once before
+        //if it has, use the results from that stored configuration
+        Configuration config = getConfiguration(players, playerIndex, map);
+        if (config.hasBeenPlayed)
+        {
+            return config.results;
+        }
+
+        //otherwise, continue executing the current configuration
+        std::vector<unsigned long long> results(players.size(), 0);
+
+        std::vector<Player> quantumPlayers = retrieveNextRoundPlayers(players[playerIndex]);
+        for (const auto& quantumPlayer : quantumPlayers)
+        {
+            if (quantumPlayer.score >= maxScore)
+            {
+                results[playerIndex]++;
+
+                continue;
+            }
+
+            //create a copy of players, and modify the active player
+            std::vector<Player> playersCopy = players;
+            playersCopy[playerIndex] = quantumPlayer;  
+
+            //mod the playerIndex:
+            // - the index needs to be in bounds for the 'players' list
+            // - active players need to be rotated, since we're not looping through the players list
+            int nextPlayerIndex = (playerIndex + 1) % players.size();
+            
+            //recurse into the next layer of games that should be played
+            auto updatedCounts = playQuantumGame(playersCopy, nextPlayerIndex, map, maxScore);
+
+            //add the updated counts to the local results variable
+            //using this method, all combined results from the 'children' are stored in the parent variable
+            for (int i = 0; i < updatedCounts.size(); i++)
+            {
+                results[i] += updatedCounts[i];
+            }
+        }      
+
+        //results are in, can be stored in the map now:
+        //- create a key/value pair for the current configuration
+        std::string key = createKey(players, playerIndex);
+        std::string value = createValue(results);
+
+        //map the results 
+        map[key] = value;
+
+        return results;
     }
 
     std::string getSolutionPart1()
@@ -173,12 +314,13 @@ private:
 
     std::string getSolutionPart2()
     {
-        int result = 0;
+        unsigned long long result = 0;
 
-        std::vector<unsigned long long> winnerCounts(data.size(), 0);
-        playQuantumGame(data, winnerCounts, 21);
+        std::map<std::string, std::string> map;
+        auto results = playQuantumGame(data, 0, map, 21);
 
-        std::cout << std::to_string(winnerCounts[0]) << ", " << std::to_string(winnerCounts[1]) << std::endl;
+        result = getHighestWinningCount(results);
+
         return std::to_string(result);
     }
 
