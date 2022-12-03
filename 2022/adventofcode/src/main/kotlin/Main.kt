ef98325e6347df1c99e1a09e04352425f4e361d1
Joris van Rheden <jvanrheden@bol.com>
2022-12-03 22:14:30 +0100

    day 3

--- 2022/adventofcode/src/main/kotlin/Main.kt
+++ 2022/adventofcode/src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 import assignments.Assignment
 import assignments.Assignment1
 import assignments.Assignment2
+import assignments.Assignment3
 import utilities.Utilities
 
 fun main(args: Array<String>) {
-    val assignment: Assignment = Assignment2()
+    val assignment: Assignment = Assignment3()
 
     val input: List<String> = Utilities.readFile("src/main/kotlin/input/" + assignment.getInput())
 
---
+++ 2022/adventofcode/src/main/kotlin/assignments/Assignment3.kt
@@ -0,0 +1,71 @@
+package assignments
+
+class Assignment3 : Assignment() {
+
+    data class Rucksack(
+        val originalEntry: String,
+        val compartmentA: String,
+        val compartmentB: String
+    )
+
+    private lateinit var rucksacks: List<Rucksack>
+
+    override fun getInput(): String {
+        return "input_3"
+    }
+
+    override fun initialize(input: List<String>) {
+        rucksacks = input.map {
+            // get the first half
+            val compartmentA = it.substring(0, it.length / 2)
+            // get the second half
+            val compartmentB = it.substring(it.length / 2, it.length)
+
+            Rucksack(it, compartmentA, compartmentB)
+        }
+    }
+
+    private fun getMatchingTypes(a: String, b: String): List<Char> {
+        return a.toSet()
+            .intersect(b.toSet())
+            .toList()
+    }
+
+    private fun getTypeScore(type: Char): Int {
+        return if (type.isUpperCase()) {
+            type - 'A' + 26 + 1
+        } else {
+            type - 'a' + 1
+        }
+    }
+
+    private fun getGroupBadge(entries: List<String>): Char {
+        var overlap = entries.first().toSet()
+        for (entry in entries) {
+            overlap = overlap.intersect(entry.toSet())
+        }
+        return overlap.first()
+    }
+
+    override fun calculateSolutionA(): String {
+        return rucksacks.sumOf {
+            getMatchingTypes(
+                it.compartmentA,
+                it.compartmentB
+            ).sumOf { x ->
+                getTypeScore(x)
+            }
+        }.toString()
+    }
+
+    override fun calculateSolutionB(): String {
+        // Create groups of 3 rucksacks
+        val groups = rucksacks.map { it.originalEntry }.chunked(3)
+
+        return groups.sumOf {
+            getTypeScore(
+                getGroupBadge(it)
+            )
+        }.toString()
+    }
+}
