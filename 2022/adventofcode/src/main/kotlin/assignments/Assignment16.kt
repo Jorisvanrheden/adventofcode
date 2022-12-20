a2d9971eda3a0915888042e6f3f5ae5bfdca8d1d
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-12-20 19:58:42 +0100

    day 16 part 1 rewrite

--- 2022/adventofcode/src/main/kotlin/assignments/Assignment16.kt
+++ 2022/adventofcode/src/main/kotlin/assignments/Assignment16.kt
@@ -1,34 +1,10 @@
 package assignments
 
+import toolkit.Matrix
+
 class Assignment16 : Assignment() {
 
     data class Valve(val name: String, val flowRate: Int, val connections: List<Int>)
-    data class State(
-        var valveOpenedStatuses: MutableList<Boolean>,
-        var visitedValves: MutableList<Int> = mutableListOf(),
-        var totalPressure: Int = 0,
-        var totalFlowRate: Int = 0,
-        var activeValveIndex: Int,
-        var previousValveIndex: Int = 0,
-        var turn: Int = 0
-    ) {
-        fun copy(): State =
-            State(
-                valveOpenedStatuses.toMutableList(),
-                visitedValves.toMutableList(),
-                totalPressure,
-                totalFlowRate,
-                activeValveIndex,
-                previousValveIndex,
-                turn
-            )
-
-        fun toKey(): String {
-            return test(turn, valveOpenedStatuses, totalPressure, totalFlowRate, activeValveIndex).hashCode().toString()
-        }
-
-        data class test(val turn: Int, val statuses: List<Boolean>, val pressure: Int, val flow: Int, val activeIndex: Int)
-    }
 
     override fun getInput(): String {
         return "input_16"
@@ -65,91 +41,117 @@ class Assignment16 : Assignment() {
         valves = input.map { parseInputToValve(it, names) }
     }
 
-    private var dp: MutableMap<String, State> = mutableMapOf()
-
-    private fun simulateState(state: State): State {
-        val key = state.toKey()
-        if (dp.size > 1000000) dp.clear()
-        if (dp.containsKey(key)) return dp[key]!!
-
-        // -- Beginning of the turn --
-        // 1.) process the pressure that is being released
-        state.totalPressure += state.totalFlowRate
-
-        // add visited valve
-        state.visitedValves.add(state.activeValveIndex)
-
-        state.turn++
-        if (state.turn == 30) return state
-
-        // -- Determine all the options at this valve --
-        // a.) open the valve (ONLY if unopened and the flowRate is bigger than 0)
-        val possibleStates = mutableListOf<State>()
-        // check the total scores of all options
-        if (!state.valveOpenedStatuses[state.activeValveIndex] && valves[state.activeValveIndex].flowRate > 0) {
-            // don't process if the flowRate is 0, no point in opening
-
-            val updatedState = state.copy()
-            // label the active valve as 'open'
-            updatedState.valveOpenedStatuses[updatedState.activeValveIndex] = true
-            updatedState.totalFlowRate += valves[updatedState.activeValveIndex].flowRate
-
-            // assign the previous valve index
-            updatedState.previousValveIndex = state.activeValveIndex
-
-            // recursively iterate while increasing the turn
-            val resultState = simulateState(updatedState)
-            possibleStates.add(resultState)
+    private fun getFlowRate(valveStatuses: MutableSet<Int>): Int {
+        var total = 0
+        for (i in valveStatuses) {
+            total += valves[i].flowRate
         }
-
-        // b.) move to an adjacent valve
-        for (valveIndex in valves[state.activeValveIndex].connections) {
-            // check if the valveIndex is not the same as the previous valve index (run in circles)
-            if (valveIndex == state.previousValveIndex) continue
-
-            val updatedState = state.copy()
-            // assign the updated valve
-            updatedState.activeValveIndex = valveIndex
-            // assign the previous valve index
-            updatedState.previousValveIndex = state.activeValveIndex
-
-            // recursively iterate while increasing the turn
-            val resultState = simulateState(updatedState)
-            possibleStates.add(resultState)
-        }
-
-        if (possibleStates.isEmpty()) {
-            return simulateState(state)
-        }
-
-        // only return the one with the highest value
-        val r = possibleStates.maxByOrNull { it.totalPressure }!!
-        if (r.totalPressure > highestTotalPressure) {
-            highestTotalPressure = r.totalPressure
-            println(highestTotalPressure)
-        }
-
-        dp[key] = r
-        return r
+        return total
     }
 
-    private var highestTotalPressure = 0
+    private fun simulateState(position: Int, turn: Int, total: Int, openSet: MutableSet<Int>): Int {
+        // -- Beginning of the turn --
+
+        // 1.) process the pressure that is being released
+        if (turn >= 30) return total
+//        var maxPossiblePressure = total + getFlowRate(openSet) * (30 - turn)
+
+        var outcomes = mutableListOf<Int>()
+
+        // Loop through all open valves and validate usage
+        for (index in usefulValves) {
+            // no need to process current valve
+            if (position == index) continue
+
+            if (openSet.contains(index)) continue
+
+            // no need to process a valve that's too far away
+            // also add 1 for the time it takes to open the valve
+            val requiredTime = distanceMatrix.values[position][index] + 1
+            if (turn + requiredTime >= 30) {
+                continue
+            }
+
+            // increment total pressure and label the valve as opened
+            val newTotal = total + requiredTime * getFlowRate(openSet)
+
+            openSet.add(index)
+
+            val outcome = simulateState(index, turn + requiredTime, newTotal, openSet)
+            outcomes.add(outcome)
+            openSet.remove(index)
+        }
+
+        return if (outcomes.isEmpty()) {
+            simulateState(position, turn + 1, total + getFlowRate(openSet), openSet)
+        } else {
+            outcomes.maxOrNull()!!
+        }
+    }
+
+    private lateinit var distanceMatrix: Matrix
+    private lateinit var usefulValves: List<Int>
+
+    private fun getUsefulValves(valves: List<Valve>): List<Int> {
+        var v = mutableListOf<Int>()
+        for ((index, s) in valves.withIndex()) {
+            if (s.flowRate > 0)v.add(index)
+        }
+        return v
+    }
+
+    private fun getDistanceMatrix(valves: List<Valve>): Matrix {
+        val matrix = Matrix(valves.size, valves.size)
+
+        for (i in valves.indices) {
+            for (j in valves.indices) {
+                // identical nodes have a distance 0
+                if (i == j) {
+                    matrix.values[i][j] = 0
+                } else {
+                    // initialize with max distance
+                    matrix.values[i][j] = valves.size
+                }
+            }
+        }
+
+        for (i in valves.indices) {
+            for (j in valves[i].connections.indices) {
+                val x = i
+                val y = valves[i].connections[j]
+                // set distance to all connected nodes
+                matrix.values[x][y] = 1
+            }
+        }
+
+        for (q in 0..valves.size) {
+            for (i in valves.indices) {
+                for (j in valves.indices) {
+                    for (k in valves.indices) {
+                        if (matrix.values[i][j] > matrix.values[i][k] + matrix.values[k][j]) {
+                            matrix.values[i][j] = matrix.values[i][k] + matrix.values[k][j]
+                        }
+                    }
+                }
+            }
+        }
+
+        return matrix
+    }
 
     override fun calculateSolutionA(): String {
+        distanceMatrix = getDistanceMatrix(valves)
+        usefulValves = getUsefulValves(valves)
         // moving to a valve takes 1 minute
         // opening a valve takes 1 minute
         // only the t(minuteOfOpeningValve) + 1 starts releasing pressure
 
         // so each action takes one minute
-        val openedStatus = valves.map { false }.toMutableList()
         val startIndex = valves.indexOfFirst { it.name == "AA" }
-        var state = State(openedStatus, activeValveIndex = startIndex)
-        val result = simulateState(state)
+        var openSet = mutableSetOf<Int>()
+        val result = simulateState(startIndex, 0, 0, openSet)
 
-        val text = result.visitedValves.map { valves[it].name }
-        println(text)
-
-        return result.totalPressure.toString()
+        return result.toString()
     }
 
     override fun calculateSolutionB(): String {
