4c2c6317701c6e3355259413b603b1eb5769a400
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-12-27 17:30:20 +0100

    day 21 part 2

--- 2022/adventofcode/src/main/kotlin/assignments/Assignment21.kt
+++ 2022/adventofcode/src/main/kotlin/assignments/Assignment21.kt
@@ -1,5 +1,10 @@
 package assignments
 
+import kotlin.math.abs
+
+var shouldPrint = false
+var lastDelta = 0L
+
 class Assignment21 : Assignment() {
 
     override fun getInput(): String {
@@ -7,37 +12,50 @@ class Assignment21 : Assignment() {
     }
 
     interface MathOperation {
-        fun getResult(monkeys: List<Monkey>): Long
+        fun getResult(monkeys: Map<String, Monkey>): Long
+        fun equals(monkeys: Map<String, Monkey>): Boolean = false
     }
     class StaticOperation(private val number: Long) : MathOperation {
-        override fun getResult(monkeys: List<Monkey>): Long = number
+        override fun getResult(monkeys: Map<String, Monkey>): Long = number
     }
     class AddOperation(private val monkeyA: String, private val monkeyB: String) : MathOperation {
-        override fun getResult(monkeys: List<Monkey>): Long =
-            monkeys.first { it.name == monkeyA }.mathOperation!!.getResult(monkeys) +
-            monkeys.first { it.name == monkeyB }.mathOperation!!.getResult(monkeys)
+        override fun getResult(monkeys: Map<String, Monkey>): Long =
+            monkeys[monkeyA]!!.mathOperation.getResult(monkeys) + monkeys[monkeyB]!!.mathOperation.getResult(monkeys)
+
+        override fun equals(monkeys: Map<String, Monkey>): Boolean {
+            val a = monkeys[monkeyA]!!.mathOperation.getResult(monkeys)
+            val b = monkeys[monkeyB]!!.mathOperation.getResult(monkeys)
+
+            if (shouldPrint) {
+                println("Comparing $a with $b")
+                shouldPrint = false
+            }
+//            println("Comparing $a with $b")
+            val delta = abs(a - b)
+            println("Delta: ${abs(lastDelta - delta)}")
+            lastDelta = delta
+
+            return a == b
+        }
     }
     class SubtractOperation(private val monkeyA: String, private val monkeyB: String) : MathOperation {
-        override fun getResult(monkeys: List<Monkey>): Long =
-            monkeys.first { it.name == monkeyA }.mathOperation!!.getResult(monkeys) -
-            monkeys.first { it.name == monkeyB }.mathOperation!!.getResult(monkeys)
+        override fun getResult(monkeys: Map<String, Monkey>): Long =
+            monkeys[monkeyA]!!.mathOperation.getResult(monkeys) - monkeys[monkeyB]!!.mathOperation.getResult(monkeys)
     }
     class MultiplyOperation(private val monkeyA: String, private val monkeyB: String) : MathOperation {
-        override fun getResult(monkeys: List<Monkey>): Long =
-            monkeys.first { it.name == monkeyA }.mathOperation!!.getResult(monkeys) *
-            monkeys.first { it.name == monkeyB }.mathOperation!!.getResult(monkeys)
+        override fun getResult(monkeys: Map<String, Monkey>): Long =
+            monkeys[monkeyA]!!.mathOperation.getResult(monkeys) * monkeys[monkeyB]!!.mathOperation.getResult(monkeys)
     }
     class DivideOperation(private val monkeyA: String, private val monkeyB: String) : MathOperation {
-        override fun getResult(monkeys: List<Monkey>): Long =
-            monkeys.first { it.name == monkeyA }.mathOperation!!.getResult(monkeys) /
-            monkeys.first { it.name == monkeyB }.mathOperation!!.getResult(monkeys)
+        override fun getResult(monkeys: Map<String, Monkey>): Long =
+            monkeys[monkeyA]!!.mathOperation.getResult(monkeys) / monkeys[monkeyB]!!.mathOperation.getResult(monkeys)
     }
 
-    data class Monkey(val name: String, var mathOperation: MathOperation?)
+    data class Monkey(val name: String, var mathOperation: MathOperation = StaticOperation(0))
 
-    private lateinit var monkeys: List<Monkey>
+    private lateinit var monkeys: Map<String, Monkey>
 
-    private fun String.toMathOperation(monkeys: List<Monkey>): MathOperation? {
+    private fun String.toMathOperation(monkeys: List<Monkey>): MathOperation {
         // split on spaces
         var chunks = this.trim().split(' ')
 
@@ -51,7 +69,7 @@ class Assignment21 : Assignment() {
                 "*" -> return MultiplyOperation(chunks[0], chunks[2])
             }
         }
-        return null
+        return StaticOperation(0)
     }
 
     private fun parseMonkey(input: String, monkeys: List<Monkey>): Monkey {
@@ -64,21 +82,64 @@ class Assignment21 : Assignment() {
 
     override fun initialize(input: List<String>) {
         var initMonkeys = input.map {
-            Monkey(it.split(':')[0], null)
+            Monkey(it.split(':')[0])
         }
 
         monkeys = input.map {
             parseMonkey(it, initMonkeys)
-        }
+        }.associateBy { it.name }
     }
 
     override fun calculateSolutionA(): String {
-        val monkey = monkeys.first { it.name == "root" }
-        return monkey.mathOperation!!.getResult(monkeys).toString()
-        return ""
+        return monkeys["root"]!!
+            .mathOperation
+            .getResult(monkeys)
+            .toString()
     }
 
     override fun calculateSolutionB(): String {
-        return ""
+        var yellValue = 3759569744800L
+
+        var its = 0
+
+        // iterate yell values for 'humn', and check which one results in a passing evaluation for 'root'
+        while (true) {
+            if (its % 100000 == 0){
+                shouldPrint = true
+                println("another hundred")
+            }
+            its++
+//            println(yellValue)
+            monkeys["humn"]!!
+                .mathOperation = StaticOperation(yellValue)
+
+            if (monkeys["root"]!!.mathOperation.equals(monkeys)) {
+                return yellValue.toString()
+            }
+
+            yellValue++
+        }
+
+        // each 2 units higher, diff is 28
+
+        // per 100
+        // a: 68634163965676
+        // b: 68634163964266
+        // diff = 1410
+
+        // per 10000
+        // a: 68634162566590
+        // b: 68634161156222
+        // diff = 1410368
+
+        // total diff
+        // 68634163976960
+        // 15610303684582
+        // 53023860292378
+
+        // 37595762,44808305350093
+        // 3759576200000,44808305350093
+
+        return "No answer found"
     }
 }
