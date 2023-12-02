74c9b1db7fad3c3c139a6224f1cbe116dacf0472
Joris van Rheden <jvanrheden@bol.com>
2023-12-02 02:32:37 +0100

    assignment 1

---
+++ 2023/adventofcode/src/main/kotlin/assignments/Assignment1.kt
@@ -0,0 +1,61 @@
+package assignments
+
+class Assignment1 : Assignment() {
+    private lateinit var lines: List<String>
+
+    override fun getInput(): String {
+        return "input_1"
+    }
+
+    override fun initialize(input: List<String>) {
+        lines = input
+    }
+
+    override fun calculateSolutionA() =
+        lines
+            .sumOf { it.calibrationValue() }
+            .toString()
+
+    override fun calculateSolutionB() =
+        lines
+            .map { it.replaceNamesWithNumbers() }
+            .sumOf { it.calibrationValue() }
+            .toString()
+
+    private fun String.replaceNamesWithNumbers(): String {
+        var line = ""
+        forEachIndexed { index, char ->
+            if (char.isDigit()) {
+                line += char
+            } else {
+                NAMES_TO_NUMBERS.forEach { (name, number) ->
+                    if (substring(index).startsWith(name)) {
+                        line += number
+                    }
+                }
+            }
+        }
+        return line
+    }
+
+    private fun String.calibrationValue() =
+        listOf(
+            first { it.isDigit() },
+            last { it.isDigit() },
+        ).joinToString("")
+            .toInt()
+
+    private companion object {
+        val NAMES_TO_NUMBERS = mapOf(
+            "one" to '1',
+            "two" to '2',
+            "three" to '3',
+            "four" to '4',
+            "five" to '5',
+            "six" to '6',
+            "seven" to '7',
+            "eight" to '8',
+            "nine" to '9',
+        )
+    }
+}
