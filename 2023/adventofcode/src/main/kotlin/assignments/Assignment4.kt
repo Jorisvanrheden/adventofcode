f1d834471e4bd385594419fea04d656a553b2fc3
Joris van Rheden <jvanrheden@bol.com>
2023-12-04 12:08:05 +0100

    cleanup assignment 4

--- 2023/adventofcode/src/main/kotlin/assignments/Assignment4.kt
+++ 2023/adventofcode/src/main/kotlin/assignments/Assignment4.kt
@@ -11,30 +11,25 @@ class Assignment4 : Assignment() {
     }
 
     override fun initialize(input: List<String>) {
-        cards = input.map { line ->
-            val chunks = line.split('|')
-
-            val winningNumbers = chunks[0]
-                .split(':')[1]
-                .trim()
-                .split(' ')
-                .filter { it.isNotEmpty() }
-                .map { it.toInt() }
-
-            val numbers = chunks[1]
-                .trim()
-                .split(' ')
-                .filter { it.isNotEmpty() }
-                .map { it.toInt() }
-
-            Card(winningNumbers, numbers)
-        }
+        cards = input
+            .map { it.split('|') }
+            .map {
+                Card(
+                    it[0].split(':')[1].parseNumbers(),
+                    it[1].parseNumbers(),
+                )
+            }
     }
+
+    private fun String.parseNumbers() =
+        split(' ')
+            .filter { it.isNotEmpty() }
+            .map { it.toInt() }
 
     override fun calculateSolutionA() =
         cards.sumOf { card ->
-            card.numbers
-                .count { card.winningNumbers.contains(it) }
+            card
+                .countWinningNumbers()
                 .let { count ->
                     if (count == 0) {
                         0
@@ -45,17 +40,18 @@ class Assignment4 : Assignment() {
         }.toString()
 
     override fun calculateSolutionB(): String {
-        val counts = mutableListOf<Int>()
-        cards.forEachIndexed { index, card -> counts.add(1) }
+        // track instances of each card
+        val cardStash = cards.map { 1 }.toMutableList()
 
         cards.forEachIndexed { index, card ->
-            // get counts of winning numbers
-            val count = card.numbers.count { card.winningNumbers.contains(it) }
-
-            for (i in (index + 1)..(index + count)) {
-                counts[i] += counts[index] * 1
+            // index + 1, because we don't want to add to the current card stash
+            for (i in 0 until card.countWinningNumbers()) {
+                cardStash[index + 1 + i] += cardStash[index]
             }
         }
-        return counts.sum().toString()
+        return cardStash.sum().toString()
     }
+
+    private fun Card.countWinningNumbers() =
+        numbers.count { winningNumbers.contains(it) }
 }
