202b6bee56c2a6886b9c63b722d2f7193c81ac5f
Joris van Rheden <joris.vanrheden@tomtom.com>
2024-12-01 09:46:07 +0100

    2024 day 1

--- 2024/src/main/kotlin/assignments/Assignment1.kt
+++ 2024/src/main/kotlin/assignments/Assignment1.kt
@@ -1,21 +1,40 @@
 package assignments
 
 import models.assignment.Assignment
+import kotlin.math.abs
 
 class Assignment1 : Assignment() {
+    private lateinit var leftList: List<Int>
+    private lateinit var rightList: List<Int>
 
     override fun getInput(): String {
         return "input_1"
     }
 
     override fun initialize(input: List<String>) {
+        input
+            .map { it.split(' ').filter { it.isNotEmpty() } }
+            .let { lines ->
+                leftList = lines.map { it[0].toInt() }
+                rightList = lines.map { it[1].toInt() }
+            }
     }
 
     override fun calculateSolutionA(): String {
-        return ""
+        val sortedLeftList = leftList.sorted()
+        val sortedRightList = rightList.sorted()
+        return sortedLeftList
+            .zip(sortedRightList)
+            .sumOf { abs(it.first - it.second) }
+            .toString()
     }
 
     override fun calculateSolutionB(): String {
-        return ""
+        return leftList
+            .sumOf { leftItem ->
+                val occurrences = rightList.count { rightItem -> leftItem == rightItem }
+                leftItem * occurrences
+            }
+            .toString()
     }
 }
